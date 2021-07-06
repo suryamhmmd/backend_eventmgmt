0c13b238deb9fded697e1147cbf656285c1713fc
M160621002 <surya.muhammad@bankmandiri.co.id>
2021-07-07 02:11:56 +0700

    first

---
+++ controller/first.py
@@ -0,0 +1,75 @@
+import numpy as np
+import random as rd
+from random import randint
+import pandas as pd
+
+def cal_fitness(weight, value, population, threshold):
+    fitness = np.empty(population.shape[0])
+    for i in range(population.shape[0]):
+        S1 = np.sum(population[i] * value)
+        S2 = np.sum(population[i] * weight)
+        if float(S2) <= float(threshold):
+            fitness[i] = 1/1+S1
+        else :
+            fitness[i] = 0 
+    return fitness.astype(int)
+
+def selection(fitness, num_parents, population):
+    fitness = list(fitness)
+    parents = np.empty((num_parents, population.shape[1]))
+    for i in range(num_parents):
+        max_fitness_idx = np.where(fitness == np.max(fitness))
+        parents[i,:] = population[max_fitness_idx[0][0], :]
+        fitness[max_fitness_idx[0][0]] = -999999
+    return parents
+
+def crossover(parents, num_offsprings):
+    offsprings = np.empty((num_offsprings, parents.shape[1]))
+    crossover_point = int(parents.shape[1]/2)
+    crossover_rate = 0.8
+    i=0
+    while (parents.shape[0] < num_offsprings):
+        parent1_index = i%parents.shape[0]
+        parent2_index = (i+1)%parents.shape[0]
+        x = rd.random()
+        if x > crossover_rate:
+            continue
+        parent1_index = i%parents.shape[0]
+        parent2_index = (i+1)%parents.shape[0]
+        offsprings[i,0:crossover_point] = parents[parent1_index,0:crossover_point]
+        offsprings[i,crossover_point:] = parents[parent2_index,crossover_point:]
+        i=+1
+    return offsprings    
+
+def mutation(offsprings):
+    mutants = np.empty((offsprings.shape))
+    mutation_rate = 0.4
+    for i in range(mutants.shape[0]):
+        random_value = rd.random()
+        mutants[i,:] = offsprings[i,:]
+        if random_value > mutation_rate:
+            continue
+        int_random_value = randint(0,offsprings.shape[1]-1)    
+        if mutants[i,int_random_value] == 0 :
+            mutants[i,int_random_value] = 1
+        else :
+            mutants[i,int_random_value] = 0
+    return mutants
+
+def optimize(weight, value, population, pop_size, num_generations, threshold):
+    parameters, fitness_history = [], []
+    num_parents = int(pop_size[0]/2)
+    num_offsprings = pop_size[0] - num_parents 
+    for i in range(num_generations):
+        fitness = cal_fitness(weight, value, population, threshold)
+        fitness_history.append(fitness)
+        parents = selection(fitness, num_parents, population)
+        offsprings = crossover(parents, num_offsprings)
+        mutants = mutation(offsprings)
+        population[0:parents.shape[0], :] = parents
+        population[parents.shape[0]:, :] = mutants
+        
+    fitness_last_gen = cal_fitness(weight, value, population, threshold)      
+    max_fitness = np.where(fitness_last_gen == np.max(fitness_last_gen))
+    parameters.append(population[max_fitness[0][0],:])
+    return parameters, fitness_history
